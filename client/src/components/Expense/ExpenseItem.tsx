f9267c1e47d6a834edd81763207a18afbf07db08
jontruong05 <jontruong98@gmail.com>
2024-11-11 15:43:04 -0800

    Working on exercises

--- client/src/components/Expense/ExpenseItem.tsx
+++ client/src/components/Expense/ExpenseItem.tsx
@@ -1,14 +1,16 @@
 import { useContext } from 'react';
 import { Expense } from "../../types/types";
 import { AppContext } from '../../context/AppContext';
+import { deleteExpense } from '../../utils/expense-utils';
 
 const ExpenseItem = (currentExpense: Expense) => {
   // Exercise: Consume the AppContext here
   const { expenses, setExpenses } = useContext(AppContext);
 
   const handleDeleteExpense = (currentExpense: Expense) => {
+    deleteExpense(currentExpense.id)
     // Exercise: Remove expense from expenses context array
-    const updatedExpenses = expenses.filter(expense => expense.id != currentExpense.id)
+    const updatedExpenses = expenses.filter(expense => expense.id !== currentExpense.id)
     setExpenses(updatedExpenses);
   };
 
